4bc663eef3a6980bf8cf482a793360db77f4962d
lucid-dreamer11 <larina0107@gmail.com>
2023-11-29 22:15:44 +1300

    new commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data';
 import './App.css';
 
-function App() {
+function App (){
+  const [films, setFilms] = useState(data);
+  const removeFilm = (id) => {
+    let watchedFilms = films.filter(film => film.id !== id);
+    setFilms(watchedFilms)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container">
+      <h1>List of {films.length} films to watch on Xmas holidays</h1>
+      </div>
+    {films.map ((element => {
+      const {id, title, year, poster, description} = element;
+      return(
+        <div key = {id}>
+          <div className='container'>
+          <h3>{id}. {title}</h3>
+          </div>
+          <div className='container'>
+          <img src={poster} width="200px" alt="film"/>
+          </div>
+          <div className='container'>
+          <h4>{year}</h4>
+          </div>
+          <div className='container'>
+          <p>{description}</p>
+          </div>
+          <div className='container'>
+          <button onClick={() => removeFilm(id)}>Remove</button>
+          </div>
+        </div>
+      )
+    }))}
+        <div className='container btn'>
+          <button onClick={() => setFilms([])}>Delete all</button>
+        </div>
     </div>
-  );
+  )
 }
-
 export default App;
---
+++ src/data.js
@@ -0,0 +1,27 @@
+//list of movies to watch over the Xmas break
+import easyvirtue from './easyvirtue.jpg';
+import beautifulmind from './beautifulmind.jpg';
+import goodwillhunting from './goodwillhunting.jpg';
+export const data = [
+    {
+        id: 1,
+        title: "Easy Virtue",
+        year: "(2008)",
+        poster: easyvirtue,
+        description: "A young Englishman marries a glamorous American. When he brings her home to meet the parents, she arrives like a blast from the future - blowing their entrenched British stuffiness out the window."
+    },
+    {
+        id: 2,
+        title: "A Beautiful Mind",
+        year: "(2001)",
+        poster: beautifulmind,
+        description: "After John Nash, a brilliant but asocial mathematician, accepts secret work in cryptography, his life takes a turn for the nightmarish."
+    },
+    {
+        id: 3,
+        title: "Good Will Hunting",
+        year: "(1997)",
+        poster: goodwillhunting,
+        description: "Will Hunting, a janitor at M.I.T., has a gift for mathematics, but needs help from a psychologist to find direction in his life."
+    }
+]
